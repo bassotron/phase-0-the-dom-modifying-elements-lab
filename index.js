d804fe161c13d158b9856a327146458c0afa50e0
Will Lovell <willard.lovell@icloud.com>
2021-11-02 14:50:32 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove("main");
+
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Will is the champion";
+
+
+
+
+
+
+
